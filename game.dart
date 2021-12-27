08d912c18d97f93a9a7d7f8e4272defbed02fcbc
Ibad Shamsi <ibadshamsi8@gmail.com>
2021-12-27 20:58:22 +0530

    new program

--- game.dart
+++ game.dart
@@ -1,67 +1,7 @@
-enum Suit {
-  spades,
-  diamonds,
-  clubs,
-  hearts,
-}
-
-final cardValues = Set<String>.from([
-  'A',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  'King',
-  'Queen',
-  'Jack',
-]);
-
-class Card {
-  final Suit suit;
-  final String value;
-  const Card(this.suit, this.value);
-
-  @override
-  bool operator ==(Object other) {
-    if (other is! Card) {
-      return false;
-    }
-    return suit == other.suit && value == other.value;
-  }
-
-  @override
-  String toString() {
-    return '"$value of ${suit.name}"';
-  }
-}
-
-List<Card> getHand(int handSize, List<Card> deck) {
-  final hand = <Card>[];
-  for (var i = 0; i < handSize; i++) {
-    final lastIndex = deck.length - 1;
-    final card = deck.removeAt(lastIndex);
-    hand.add(card);
-  }
-  return hand;
-}
+import 'dart:io';
 
 void main(List<String> args) {
-  final deck = <Card>[];
-  for (var suit in Suit.values) {
-    for (var i in cardValues) {
-      final card = Card(suit, i);
-      deck.add(card);
-      print(card.toString());
-    }
-  }
-  deck.shuffle();
-  print(deck);
-  final hand = getHand(5, deck);
-  print('Hand: $hand');
-  print('Remaining cards in deck: $deck');
+  print("whats your name:");
+  var name = stdin.readLineSync();
+  print("Hi $name");
 }
