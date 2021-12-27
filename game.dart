4031d4827da5122280bfede693a37ae8357ad1ea
Ibad Shamsi <ibadshamsi8@gmail.com>
2021-12-27 15:13:22 +0530

    adding enum values and card values

--- game.dart
+++ game.dart
@@ -1,4 +1,23 @@
-void main() {
-  var list1 = [10, 20, 30, 40, 50, 60];
-  for (var i in list1) print(i);
+enum Suit {
+  spades,
+  diamonds,
+  clubs,
+  hearts,
 }
+
+final cardValues = <String>[
+  'A',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'King',
+  'Queen',
+  'Jack',
+];
+void main(List<String> args) {}
