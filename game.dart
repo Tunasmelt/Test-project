b967561ca70b8099739eb189e063b61bc1096003
Ibad Shamsi <ibadshamsi8@gmail.com>
2021-12-29 13:40:50 +0530

    Input Func

--- game.dart
+++ game.dart
@@ -1,7 +1,14 @@
 import 'dart:io';
 
+String? input(String message) {
+  stdout.write(message);
+  return stdin.readLineSync();
+}
+
 void main(List<String> args) {
-  print("whats your name:");
-  var name = stdin.readLineSync();
-  print("Hi $name");
+  final a = input('Enter a number: ');
+  final b = int.tryParse(a ?? '');
+  if (b == null) {
+    throw UnsupportedError('Invalid input');
+  }
 }
